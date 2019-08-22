68e993d57caed529c091d0ab163e01ca99e51531
Rikard Hevosmaa <r.hevosmaa@posteo.se>
2019-08-22 02:37:27 +0200

    Add face configurations for the eshell prompt
    Change the colour of the eshell prompt from the default pink to cyan,
    which fits the general colour scheme better.

--- lisp/faces.el
+++ lisp/faces.el
@@ -2,39 +2,39 @@
 
 (when (eq system-type 'darwin)
   (if (member "SF Mono" (font-family-list))
-      (set-face-attribute 'default nil :font "SF Mono-15")
-    (set-face-attribute 'default nil :height 150)))
+      (set-face-attribute 'default nil :font "SF Mono-17")
+    (set-face-attribute 'default nil :height 170)))
 
 (when (display-graphic-p)
   ;; Defaults.
   (set-face-attribute 'default nil
-                      :foreground "#BEBEBE"
+                      :foreground "#bebebe"
                       :background "#000000")
   (set-face-attribute 'cursor nil
                       :foreground nil
-                      :background "#90EE90")
+                      :background "#90ee90")
   (set-face-attribute 'region nil
                       :foreground nil
-                      :background "#0000FF")
+                      :background "#0000ff")
   (set-face-attribute 'highlight nil
                       :foreground nil
-                      :background "#0000FF")
+                      :background "#0000ff")
   (set-face-attribute 'error nil
-                      :foreground "#FF0000"
+                      :foreground "#ff0000"
                       :bold t)
-  
+
   ;; Mode line.
   (set-face-attribute 'mode-line nil
                       :foreground "#000000"
-                      :background "#BEBEBE"
+                      :background "#bebebe"
                       :box nil)
   (set-face-attribute 'mode-line-inactive nil
-                      :foreground "#BEBEBE"
+                      :foreground "#bebebe"
                       :background "#303030"
                       :box nil)
   (set-face-attribute 'mode-line-buffer-id nil
                       :foreground "#000000"
-                      :distant-foreground "#BEBEBE"
+                      :distant-foreground "#bebebe"
                       :bold t)
   
   ;; Line numbers.
@@ -42,18 +42,26 @@
                       :foreground "#606060"
                       :background "#000000")
   (set-face-attribute 'line-number-current-line nil
-                      :foreground "#FFFFFF"
+                      :foreground "#ffffff"
                       :background "#000000")
   
   ;; Syntax highlighting.
-  (set-face-foreground 'font-lock-builtin-face "#FFFFFF")
-  (set-face-foreground 'font-lock-comment-delimiter-face "#67CD5D")
-  (set-face-foreground 'font-lock-comment-face "#67CD5D")
-  (set-face-foreground 'font-lock-constant-face "#8FE1C8")
-  (set-face-foreground 'font-lock-doc-face "#2EC09C")
-  (set-face-foreground 'font-lock-function-name-face "#D4D4D4")
-  (set-face-foreground 'font-lock-keyword-face "#FFFFFF")
-  (set-face-foreground 'font-lock-preprocessor-face "#86E08F")
-  (set-face-foreground 'font-lock-string-face "#2EC09C")
-  (set-face-foreground 'font-lock-type-face "#86E08F")
-  (set-face-foreground 'font-lock-variable-name-face "#D4D4D4"))
+  (set-face-foreground 'font-lock-builtin-face "#ffffff")
+  (set-face-foreground 'font-lock-comment-delimiter-face "#67cd5d")
+  (set-face-foreground 'font-lock-comment-face "#67cd5d")
+  (set-face-foreground 'font-lock-constant-face "#8fe1c8")
+  (set-face-foreground 'font-lock-doc-face "#2ec09c")
+  (set-face-foreground 'font-lock-function-name-face "#d4d4d4")
+  (set-face-foreground 'font-lock-keyword-face "#ffffff")
+  (set-face-foreground 'font-lock-preprocessor-face "#86e08f")
+  (set-face-foreground 'font-lock-string-face "#2ec09c")
+  (set-face-foreground 'font-lock-type-face "#86e08f")
+  (set-face-foreground 'font-lock-variable-name-face "#d4d4d4")
+
+  ;; Eshell prompt.
+  (setq eshell-prompt-function
+        (lambda nil
+          (concat
+           (propertize (eshell/pwd) 'face `(:foreground "#00ffff"))
+           (propertize " $ " 'face `(:foreground "#bebebe")))))
+  (setq eshell-highlight-prompt nil))
