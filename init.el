c68dd1e84facc2d698ad6894b547be94ad16f266
Rikard Hevosmaa <rikard@hevosmaa.net>
2020-06-15 21:51:17 +0200

    Update copyright; Stop loading faces.el

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;;; init.el --- Emacs initialisation file -*- lexical-binding:t -*-
 
-;; Copyright (C) 2014-2019 Rikard Hevosmaa
+;; Copyright (C) 2013-2020 Rikard Hevosmaa
 
 ;; Use of this source code is governed by the GNU General Public License
 ;; version 3 or later, which can be found in the LICENSE file.
@@ -8,7 +8,7 @@
 (when (display-graphic-p)
   (tool-bar-mode 0)
   (scroll-bar-mode 0)
-  (set-frame-size (selected-frame) 82 64))
+  (set-frame-size (selected-frame) 100 80))
 
 ;; Disable the menu bar in text consoles.
 (unless (display-graphic-p)
@@ -16,6 +16,9 @@
 
 ;; Load submodules.
 (load "~/.emacs.d/lisp/packages.el")
-(load "~/.emacs.d/lisp/faces.el")
 (load "~/.emacs.d/lisp/keybindings.el")
 (load "~/.emacs.d/lisp/misc.el")
+
+;; Disabled submodules.
+;; (load "~/.emacs.d/lisp/faces.el")
+;; (load "~/.emacs.d/lisp/email.el")
