90ba334fa2fedeaa9a86ad3360824474a8553e67
Rikard Hevosmaa <rikard@hevosmaa.net>
2022-05-20 21:03:23 +0200

    Minor fixes.
    Previous settings in init.el made it so that the menu bar never loaded,
    however we want it to load when Emacs is run with the GUI. The menu bar
    should not be loaded when Emacs is run in a text console.
    
    Update the copyright from 2013-2020 to 2013-2022.

--- init.el
+++ init.el
@@ -1,13 +1,12 @@
 ;;; init.el --- Emacs initialisation file -*- lexical-binding:t -*-
 
-;; Copyright (C) 2013-2020 Rikard Hevosmaa
+;; Copyright (C) 2013-2022 Rikard Hevosmaa
 
 ;; Use of this source code is governed by the GNU General Public License
 ;; version 3 or later, which can be found in the LICENSE file.
 
 (when (display-graphic-p)
   (tool-bar-mode 0)
-  (menu-bar-mode 0)
   (scroll-bar-mode 0))
 
 ;; Disable the menu bar in text consoles.
