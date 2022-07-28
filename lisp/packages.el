443153271b48a3c81eeffe2a7461f1c5cfc3b17c
Rikard Hevosmaa <rikard@hevosmaa.net>
2022-07-28 06:43:51 +0200

    Add font for Linux; add `magit' and `auto-complete' to list of required packages.

--- lisp/packages.el
+++ lisp/packages.el
@@ -8,7 +8,8 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
-(defvar rha/required-packages '(markdown-mode go-mode rust-mode)
+(defvar rha/required-packages
+  '(markdown-mode go-mode rust-mode magit auto-complete)
   "List of packages that should always be installed.")
 (dolist (pkg rha/required-packages)
   (when (not (package-installed-p pkg))
@@ -24,8 +25,16 @@
   (rha/append-to-path "/usr/local/bin"))
 
 ;; Use markdown-mode when editing Markdown files.
+(require 'markdown-mode)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown'" . markdown-mode))
 (setq markdown-command "multimarkdown")
+
+(require 'magit)
+
+(require 'auto-complete)
+(require 'auto-complete-config)
+(ac-config-default)
+(setq ac-use-menu-map t)
 
 ;;; packages.el ends here
